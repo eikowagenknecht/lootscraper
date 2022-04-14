0d46c45878f2777e628269a0d30389899c09586f
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2022-04-14 20:41:32 +0200

    Switch to SQLAlchemy (with old db format)
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

--- app/sqlalchemy.py
+++ app/sqlalchemy.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from datetime import datetime, timezone
 
 from pathlib import Path
 from types import TracebackType
@@ -7,14 +8,16 @@ from typing import Any, Type
 # from sqlalchemy.orm import relationship
 # from sqlalchemy import insert
 # from sqlalchemy.future.engine import Connection
-from sqlalchemy import create_engine  # , text
+from sqlalchemy import and_, create_engine  # , text
 from sqlalchemy import Column, Integer, MetaData, String, select  # , Table
 
 # from sqlalchemy import and_, or_
 # from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Session, registry
+from app.common import LootOffer, OfferType, Source
 
 from app.configparser import Config
+from app.scraper.info.gameinfo import Gameinfo
 
 mapper_registry = registry()
 Base = mapper_registry.generate_base()  # type: Any
@@ -96,3 +99,131 @@ class OldLootDatabase:
     def read_all(self) -> list[OldDbLoot]:
         result = self.session.execute(select(OldDbLoot)).scalars().all()
         return result
+
+    def read_all_segmented(self) -> dict[str, dict[str, list[LootOffer]]]:
+        result = self.read_all()
+
+        offers: dict[str, dict[str, list[LootOffer]]] = {}
+
+        for row in result:
+            offer = self.get_loot_offer_from_db_row(row)
+
+            source: str = Source(row.source).name
+            type: str = OfferType(row.type).name
+            if source not in offers:
+                offers[source] = {}
+            if type not in offers[source]:
+                offers[source][type] = []
+
+            offers[source][type].append(offer)
+
+        return offers
+
+    def find_offer(
+        self,
+        source: str | None,
+        title: str | None,
+        subtitle: str | None,
+        valid_to: datetime | None,
+    ) -> OldDbLoot:
+        statement = select(OldDbLoot).where(
+            and_(
+                OldDbLoot.source == source,
+                OldDbLoot.title == title,
+                OldDbLoot.subtitle == subtitle,
+                OldDbLoot.valid_to == valid_to.replace(tzinfo=timezone.utc).isoformat()
+                if valid_to
+                else None,
+            )
+        )
+        result = self.session.execute(statement).scalars().one_or_none()
+
+        return result
+
+    def find_offer_by_id(self, id: int) -> OldDbLoot:
+        statement = select(OldDbLoot).where(OldDbLoot.id == id)
+        result = self.session.execute(statement).scalars().one_or_none()
+
+        return result
+
+    def get_loot_offer_from_db_row(self, db_row: OldDbLoot) -> LootOffer:
+        gameinfo: Gameinfo | None = (
+            Gameinfo.from_json(db_row.gameinfo) if db_row.gameinfo else None
+        )
+        offer = LootOffer(
+            id=db_row.id,
+            source=Source(db_row.source),
+            type=OfferType(db_row.type),
+            title=db_row.title,
+            subtitle=db_row.subtitle,
+            publisher=db_row.publisher,
+            valid_from=datetime.fromisoformat(db_row.valid_from).replace(
+                tzinfo=timezone.utc
+            )
+            if db_row.valid_from
+            else None,
+            valid_to=datetime.fromisoformat(db_row.valid_to).replace(
+                tzinfo=timezone.utc
+            )
+            if db_row.valid_to
+            else None,
+            seen_first=datetime.fromisoformat(db_row.seen_first).replace(
+                tzinfo=timezone.utc
+            )
+            if db_row.seen_first
+            else None,
+            seen_last=datetime.fromisoformat(db_row.seen_last).replace(
+                tzinfo=timezone.utc
+            )
+            if db_row.seen_last
+            else None,
+            url=db_row.url,
+            img_url=db_row.img_url,
+            gameinfo=gameinfo,
+        )
+
+        return offer
+
+    def touch_db_row(self, db_row: OldDbLoot) -> None:
+        db_row.seen_last = datetime.now().replace(tzinfo=timezone.utc).isoformat()
+
+    def update_db_row_with_loot_offer(
+        self, offer: LootOffer, db_row: OldDbLoot
+    ) -> None:
+        db_row.rawtext = offer.rawtext or None
+        db_row.source = offer.source.value if offer.source else None
+        db_row.type = offer.type.value if offer.type else None
+        db_row.title = offer.title or None
+        db_row.subtitle = offer.subtitle or None
+        db_row.publisher = offer.publisher or None
+        db_row.valid_from = (
+            offer.valid_from.replace(tzinfo=timezone.utc).isoformat()
+            if offer.valid_from
+            else None
+        )
+        db_row.valid_to = (
+            offer.valid_to.replace(tzinfo=timezone.utc).isoformat()
+            if offer.valid_to
+            else None
+        )
+        db_row.url = offer.url or None
+        db_row.img_url = offer.img_url or None
+        db_row.gameinfo = offer.gameinfo.to_json() if offer.gameinfo else None
+        db_row.seen_last = datetime.now().replace(tzinfo=timezone.utc).isoformat()
+
+    def add_loot_offer(self, offer: LootOffer) -> None:
+        db_row = OldDbLoot()
+        self.update_db_row_with_loot_offer(offer, db_row)
+
+        current_date = datetime.now().replace(tzinfo=timezone.utc).isoformat()
+        db_row.seen_first = current_date
+        db_row.seen_last = current_date
+
+        self.session.add(db_row)
+
+    def update_loot_offer(self, offer: LootOffer) -> None:
+        if offer.id is None:
+            return
+
+        db_row = self.find_offer_by_id(offer.id)
+        self.update_db_row_with_loot_offer(offer, db_row)
