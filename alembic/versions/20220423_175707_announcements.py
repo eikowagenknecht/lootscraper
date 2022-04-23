c9b0f7eeac48f8d585fdc8b922a9558b2817ff85
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2022-04-23 19:58:08 +0200

    DB table for announcements
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

---
+++ alembic/versions/20220423_175707_announcements.py
@@ -0,0 +1,44 @@
+"""Announcements
+
+Revision ID: fc43de437432
+Revises: 52ea632ee417
+Create Date: 2022-04-23 17:57:07.548114+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+from app.sqlalchemy import AwareDateTime
+
+
+# revision identifiers, used by Alembic.
+revision = "fc43de437432"
+down_revision = "52ea632ee417"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "announcements",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column(
+            "channel",
+            sa.Enum("ALL", "FEED", "TELEGRAM", name="channel"),
+            nullable=False,
+        ),
+        sa.Column("date", AwareDateTime(), nullable=False),
+        sa.Column("text_markdown", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    with op.batch_alter_table("users", schema=None) as batch_op:  # type: ignore
+        batch_op.add_column(
+            sa.Column("last_announcement_id", sa.Integer(), nullable=False)
+        )
+
+
+def downgrade() -> None:
+    with op.batch_alter_table("users", schema=None) as batch_op:  # type: ignore
+        batch_op.drop_column("last_announcement_id")
+
+    op.drop_table("announcements")
