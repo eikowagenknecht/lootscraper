7ccee534acdbd81d0b4d3aef8a3e209e6f321254
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2022-06-12 21:42:24 +0200

    Split steam offers into games and loot
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

--- app/scraper/info/utils.py
+++ app/scraper/info/utils.py
@@ -57,3 +57,48 @@ def clean_nones(value: dict[str, Any]) -> dict[str, Any]:
         return {key: clean_nones(val) for key, val in value.items() if val is not None}
     else:
         return value
+
+
+def clean_game_title(title: str) -> str:
+    probable_game_name: str | None = None
+
+    probable_game_name = (
+        title.removesuffix(" on Origin")
+        .removesuffix(" Game of the Year Edition Deluxe")
+        .removesuffix(" Game of the Year Edition")
+    )
+
+    return probable_game_name
+
+
+def clean_loot_title(title: str) -> str:
+    # Unfortunately Amazon loot offers come in free text format, so we
+    # need to do some manual matching.
+    # - Most of the time, it is the part before the first ": ", e.g.
+    #   "Lords Mobile: Warlord Pack" -> Lords Mobile
+    # - When the title itself contains a ": ", it can also be the second, e.g.
+    #   "Mobile Legends: Bang Bang: Amazon Prime Chest" -> Mobile Legends: Bang Bang
+    # . Sometimes it also ist "Get ... in [Game]", e.g.
+    #   "Get up to GTA$400,000 this month in GTA Online" -> GTA Online
+    # So as a general rule, we try splitting by the second colon first,
+    # then the "Get ... in [Game] pattern" (to catch games with a colon
+    # in the name) and finally the ": " pattern.
+    #
+    # We use the same method for Steam loot offers for now as they also seem to
+    # be seperated in the same fashion.
+    probable_game_name: str | None = None
+
+    title = title.replace("：", ": ")  # Replace unicode colon with normal colon (Steam)
+    title_parts: list[str] = title.split(": ")
+    if len(title_parts) >= 3:
+        probable_game_name = ": ".join(title_parts[:-1])
+    if probable_game_name is None:
+        match = re.compile(r"Get .* in (.*)").match(title)
+        if match and match.group(1):
+            probable_game_name = match.group(1)
+    if probable_game_name is None and len(title_parts) == 2:
+        probable_game_name = ": ".join(title_parts[:-1])
+    if probable_game_name is None:
+        probable_game_name = title
+
+    return probable_game_name
