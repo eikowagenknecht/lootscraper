be15c55992b88f9792d1e2521c9f4e72a7d80fc9
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2022-06-19 20:05:25 +0200

    Refactor main
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

--- lootscraper.py
+++ lootscraper.py
@@ -32,14 +32,31 @@ EXAMPLE_CONFIG_FILE = "config.default.ini"
 
 
 def main() -> None:
-    # First thing to do: Copy the config file to the data directory if there is
-    # not yet a config file there!
+    initialize_config_file()
+    check_config_file()
+    setup_logging()
+
+    logging.info(f"Starting LootScraper v{CURRENT_VERSION}")
+    run_main_loop()
+    logging.info(f"Exiting LootScraper v{CURRENT_VERSION}")
+
+
+def initialize_config_file() -> None:
+    """
+    Copy the config file to the data directory if there is not yet a config file there!
+    """
     config_file = Config.config_file()
     if not config_file.is_file():
-        print(f"Config file {config_file} not found, creating a new one")
-        config_file.parent.mkdir(exist_ok=True, parents=True)
-        shutil.copy(EXAMPLE_CONFIG_FILE, config_file)
+        create_config_file(config_file)
 
+
+def create_config_file(config_file: Path) -> None:
+    print(f"Config file {config_file} not found, creating a new one")
+    config_file.parent.mkdir(exist_ok=True, parents=True)
+    shutil.copy(EXAMPLE_CONFIG_FILE, config_file)
+
+
+def check_config_file() -> None:
     # Now we can try to read the config file. In case anything goes wrong, we
     # terminate because without a valid config continuing is useless.
     try:
@@ -48,6 +65,8 @@ def main() -> None:
         print(f"Config could not be loaded, Keys not found: {e.args}")
         sys.exit()
 
+
+def setup_logging() -> None:
     filename = Config.data_path() / Path(Config.get().log_file)
     loglevel = Config.get().log_level
     logging.basicConfig(
@@ -62,33 +81,33 @@ def main() -> None:
         logging.Formatter("%(asctime)s %(name)s [%(levelname)-5s] %(message)s")
     )
     logging.getLogger().addHandler(stream_handler)
-    logging.info(f"Starting LootScraper v{CURRENT_VERSION}")
 
+
+def run_main_loop() -> None:
+    """
+    Run the job every hour (or whatever is set in the config file). This is
+    not exact because it does not account for the execution time, but that
+    doesn't matter in our context.
+    """
     if Config.get().virtual_linux_display:
         from xvfbwrapper import Xvfb
-
-        vdisplay = Xvfb()
-        vdisplay.start()
 
     with (
         LootDatabase(echo=Config.get().db_echo) as db,
         TelegramBot(Config.get(), db.Session) as bot,
         Xvfb() if Config.get().virtual_linux_display else nullcontext(),
     ):
-        # Run the job every hour (or whatever is set in the config file). This is
-        # not exact because it does not account for the execution time, but that
-        # doesn't matter in our context.
+
         run = 1
+        time_between_runs = int(Config.get().wait_between_runs)
+
         while not exit.is_set():
             logging.info(f"Starting Run # {run}")
 
             try:
                 job(db)
                 if Config.get().telegram_bot:
-                    session: Session = db.Session()
-                    for user in session.execute(select(User)).scalars().all():
-                        bot.send_new_announcements(user)
-                        bot.send_new_offers(user)
+                    telegram_job(db, bot)
             except OperationalError as oe:
                 logging.error(f"Database error: {oe}")
                 logging.error("Database error, exiting applications")
@@ -97,9 +116,9 @@ def main() -> None:
                 # Something unexpected occurred, log it and continue with the next run as usual
                 logging.exception(e)
 
-            time_between_runs = int(Config.get().wait_between_runs)
             if time_between_runs == 0:
                 break
+
             next_execution = datetime.now() + timedelta(seconds=time_between_runs)
 
             logging.info(
@@ -109,15 +128,7 @@ def main() -> None:
             run += 1
             exit.wait(time_between_runs)
 
-    if Config.get().virtual_linux_display:
-        vdisplay.stop()
-
-    logging.info(f"Exiting script after {run} runs")
-
-
-def quit(signo: int, _frame: FrameType | None) -> None:
-    print(f"Interrupted by signal {signo}, shutting down")
-    exit.set()
+        logging.info(f"Finished {run} runs")
 
 
 def job(db: LootDatabase) -> None:
@@ -126,159 +137,199 @@ def job(db: LootDatabase) -> None:
     cfg = Config.get()
 
     with get_pagedriver() as webdriver:
-        scraped_offers: list[Offer] = []
-        for scraper in get_all_scrapers():
-            if (
-                scraper.get_type() in cfg.enabled_offer_types
-                and scraper.get_duration() in cfg.enabled_offer_durations
-                and scraper.get_source() in cfg.enabled_offer_sources
-            ):
-                scraper_type = scraper.get_type().value
-                scraper_duration = scraper.get_duration().value
-                scraper_source = scraper.get_source().value
+        scraped_offers = scrape_offers(webdriver)
+        process_new_offers(db, webdriver, session, scraped_offers)
 
-                logging.info(
-                    f"Analyzing {scraper_source} for offers: {scraper_type} / {scraper_duration}."
-                )
-                scraper_results = scraper.scrape(webdriver)
+        all_offers = db.read_all()
 
-                if scraper_results:
-                    titles = ", ".join([offer.title for offer in scraper_results])
-                    logging.info(f"Found {len(scraper_results)} offers: {titles}.")
-                    scraped_offers.extend(scraper_results)
-                else:
-                    logging.warning("Scraper finished without results.")
-
-        # Check which offers are new and which are updated, then act accordingly:
-        # - Offers that are neither new nor updated just get a new date
-        # - Offers that are new are inserted
-        # - Offers that are updated are updated
-        nr_of_new_offers: int = 0
-
-        new_offer_titles: list[str] = []
-
-        for scraped_offer in scraped_offers:
-            # Get the existing entry if there is one
-            existing_entry: Offer | None = db.find_offer(
-                scraped_offer.source,
-                scraped_offer.type,
-                scraped_offer.title,
-                scraped_offer.valid_to,
-            )
-
-            if not existing_entry:
-                # Create a list of new scraped offers (logging only)
-                if scraped_offer.title:
-                    new_offer_titles.append(scraped_offer.title)
-
-                # The enddate has been changed or it is a new offer,
-                # get information about it (if it's a game)
-                # and insert it into the database
-                if cfg.scrape_info:
-                    add_game_info(scraped_offer, session, webdriver)
-                db.add_offer(scraped_offer)
-                nr_of_new_offers += 1
-            else:
-                # Do not insert offers that already have been scraped,
-                # but update them instead. What gets updated depends on
-                # the settings
-                if cfg.force_update:
-                    db.update_db_offer(existing_entry, scraped_offer)
-                else:
-                    db.touch_db_offer(existing_entry)
-
-        if new_offer_titles:
-            logging.info(
-                f'Found {nr_of_new_offers} new offers: {", ".join(new_offer_titles)}'
-            )
-
-        loot_offers_in_db = db.read_all()
-
-        # Refresh game information if ForceUpdate is set
         if cfg.force_update and cfg.scrape_info:
-            # Remove all game info first
-            logging.info("Force update enabled - removing all game info")
-            for game in session.query(Game):
-                session.delete(game)
-            # Then add new game info
-            for db_offer in loot_offers_in_db:
-                add_game_info(db_offer, session, webdriver)
+            rebuild_game_infos(webdriver, session, all_offers)
 
     if cfg.generate_feed:
-        feed_file_base = Config.data_path() / Path(cfg.feed_file_prefix + ".xml")
-
-        # Generate and upload feeds split by source
-        any_feed_changed = False
-
-        source: Source
-        type_: OfferType
-        duration: OfferDuration
-
-        for source, type_, duration in [
-            (x.get_source(), x.get_type(), x.get_duration()) for x in get_all_scrapers()
-        ]:
-            offers = [
-                offer
-                for offer in loot_offers_in_db
-                if offer.source == source
-                and offer.type == type_
-                and offer.duration == duration
-            ]
-            if not offers:
-                continue
-
-            feed_changed = False
-            feed_file_core = f"_{source.name.lower()}" + f"_{type_.name.lower()}"
-            # To keep the old names only add when the type is one of the new ones (!= permanent)
-            if duration != OfferDuration.CLAIMABLE:
-                feed_file_core += f"_{duration.name.lower()}"
-            feed_file = Config.data_path() / Path(
-                cfg.feed_file_prefix + feed_file_core + ".xml"
-            )
-            old_hash = hash_file(feed_file)
-            generate_feed(
-                offers=offers,
-                file=feed_file,
-                author_name=cfg.feed_author_name,
-                author_web=cfg.feed_author_web,
-                author_mail=cfg.feed_author_mail,
-                feed_url_prefix=cfg.feed_url_prefix,
-                feed_url_alternate=cfg.feed_url_alternate,
-                feed_id_prefix=cfg.feed_id_prefix,
-                source=source,
-                type=type_,
-                duration=duration,
-            )
-            new_hash = hash_file(feed_file)
-            if old_hash != new_hash:
-                feed_changed = True
-                any_feed_changed = True
-
-            if feed_changed and cfg.upload_feed:
-                upload_to_server(feed_file)
-
-        # Generate and upload cumulated feed
-        if any_feed_changed:
-            feed_file = Config.data_path() / Path(cfg.feed_file_prefix + ".xml")
-            generate_feed(
-                offers=loot_offers_in_db,
-                file=feed_file,
-                author_name=cfg.feed_author_name,
-                author_web=cfg.feed_author_web,
-                author_mail=cfg.feed_author_mail,
-                feed_url_prefix=cfg.feed_url_prefix,
-                feed_url_alternate=cfg.feed_url_alternate,
-                feed_id_prefix=cfg.feed_id_prefix,
-            )
-            if cfg.upload_feed:
-                upload_to_server(feed_file_base)
-            else:
-                logging.info("Skipping upload, disabled")
-
+        action_generate_feed(all_offers)
     else:
         logging.info("Skipping feed generation, disabled")
 
     session.commit()
+
+
+def scrape_offers(webdriver: WebDriver) -> list[Offer]:
+    cfg = Config.get()
+
+    scraped_offers: list[Offer] = []
+    for scraper in get_all_scrapers():
+        if (
+            scraper.get_type() in cfg.enabled_offer_types
+            and scraper.get_duration() in cfg.enabled_offer_durations
+            and scraper.get_source() in cfg.enabled_offer_sources
+        ):
+            scraper_type = scraper.get_type().value
+            scraper_duration = scraper.get_duration().value
+            scraper_source = scraper.get_source().value
+
+            logging.info(
+                f"Analyzing {scraper_source} for offers: {scraper_type} / {scraper_duration}."
+            )
+            scraper_results = scraper.scrape(webdriver)
+
+            if scraper_results:
+                titles = ", ".join([offer.title for offer in scraper_results])
+                logging.info(f"Found {len(scraper_results)} offers: {titles}.")
+                scraped_offers.extend(scraper_results)
+            else:
+                logging.warning("Scraper finished without results.")
+
+    return scraped_offers
+
+
+def process_new_offers(
+    db: LootDatabase,
+    webdriver: WebDriver,
+    session: Session,
+    scraped_offers: list[Offer],
+) -> None:
+    """
+    Check which offers are new and which are updated, then act accordingly:
+     - Offers that are neither new nor updated just get a new date
+     - Offers that are new are inserted
+     - Offers that are updated are updated
+    """
+
+    cfg = Config.get()
+
+    nr_of_new_offers: int = 0
+    new_offer_titles: list[str] = []
+
+    for scraped_offer in scraped_offers:
+        # Get the existing entry if there is one
+        existing_entry: Offer | None = db.find_offer(
+            scraped_offer.source,
+            scraped_offer.type,
+            scraped_offer.title,
+            scraped_offer.valid_to,
+        )
+
+        if not existing_entry:
+            # Create a list of new scraped offers (logging only)
+            if scraped_offer.title:
+                new_offer_titles.append(scraped_offer.title)
+
+                # The enddate has been changed or it is a new offer,
+                # get information about it (if it's a game)
+                # and insert it into the database
+            if cfg.scrape_info:
+                add_game_info(scraped_offer, session, webdriver)
+            db.add_offer(scraped_offer)
+            nr_of_new_offers += 1
+        else:
+            # Do not insert offers that already have been scraped,
+            # but update them instead. What gets updated depends on
+            # the settings
+            if cfg.force_update:
+                db.update_db_offer(existing_entry, scraped_offer)
+            else:
+                db.touch_db_offer(existing_entry)
+
+    if new_offer_titles:
+        logging.info(
+            f'Found {nr_of_new_offers} new offers: {", ".join(new_offer_titles)}'
+        )
+
+
+def telegram_job(db: LootDatabase, bot: TelegramBot) -> None:
+    session: Session = db.Session()
+    for user in session.execute(select(User)).scalars().all():
+        bot.send_new_announcements(user)
+        bot.send_new_offers(user)
+
+
+def rebuild_game_infos(
+    webdriver: WebDriver, session: Session, all_offers: list[Offer]
+) -> None:
+    # Remove all game info first
+    logging.info("Force update enabled - removing all game info")
+    for game in session.query(Game):
+        session.delete(game)
+
+    # Then add new game info
+    for db_offer in all_offers:
+        add_game_info(db_offer, session, webdriver)
+
+
+def action_generate_feed(loot_offers_in_db: list[Offer]) -> None:
+    cfg = Config.get()
+    feed_file_base = Config.data_path() / Path(cfg.feed_file_prefix + ".xml")
+
+    any_feed_changed = False
+
+    source: Source
+    type_: OfferType
+    duration: OfferDuration
+
+    # Generate and upload feeds split by source
+    for source, type_, duration in [
+        (x.get_source(), x.get_type(), x.get_duration()) for x in get_all_scrapers()
+    ]:
+        offers = [
+            offer
+            for offer in loot_offers_in_db
+            if offer.source == source
+            and offer.type == type_
+            and offer.duration == duration
+        ]
+        if not offers:
+            continue
+
+        feed_changed = False
+        feed_file_core = f"_{source.name.lower()}" + f"_{type_.name.lower()}"
+
+        # To keep the old feed ids and names only add when the type is one of
+        # the new types.
+        if duration != OfferDuration.CLAIMABLE:
+            feed_file_core += f"_{duration.name.lower()}"
+
+        feed_file = Config.data_path() / Path(
+            cfg.feed_file_prefix + feed_file_core + ".xml"
+        )
+        old_hash = hash_file(feed_file)
+        generate_feed(
+            offers=offers,
+            file=feed_file,
+            author_name=cfg.feed_author_name,
+            author_web=cfg.feed_author_web,
+            author_mail=cfg.feed_author_mail,
+            feed_url_prefix=cfg.feed_url_prefix,
+            feed_url_alternate=cfg.feed_url_alternate,
+            feed_id_prefix=cfg.feed_id_prefix,
+            source=source,
+            type=type_,
+            duration=duration,
+        )
+        new_hash = hash_file(feed_file)
+        if old_hash != new_hash:
+            feed_changed = True
+            any_feed_changed = True
+
+        if feed_changed and cfg.upload_feed:
+            upload_to_server(feed_file)
+
+    # Generate and upload cumulated feed
+    if any_feed_changed:
+        feed_file = Config.data_path() / Path(cfg.feed_file_prefix + ".xml")
+        generate_feed(
+            offers=loot_offers_in_db,
+            file=feed_file,
+            author_name=cfg.feed_author_name,
+            author_web=cfg.feed_author_web,
+            author_mail=cfg.feed_author_mail,
+            feed_url_prefix=cfg.feed_url_prefix,
+            feed_url_alternate=cfg.feed_url_alternate,
+            feed_id_prefix=cfg.feed_id_prefix,
+        )
+        if cfg.upload_feed:
+            upload_to_server(feed_file_base)
+        else:
+            logging.info("Skipping upload, disabled")
 
 
 def add_game_info(offer: Offer, session: Session, webdriver: WebDriver) -> None:
@@ -381,6 +432,11 @@ def hash_file(file: Path) -> str:
             hash.update(data)
 
     return hash.hexdigest()
+
+
+def quit(signo: int, _frame: FrameType | None) -> None:
+    print(f"Interrupted by signal {signo}, shutting down")
+    exit.set()
 
 
 if __name__ == "__main__":
