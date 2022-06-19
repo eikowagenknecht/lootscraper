91290f13469072a2fa522f125c36b79dcaabc0f9
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2022-06-19 16:32:59 +0200

    Add itch.io
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

--- app/scraper/loot/scraperhelper.py
+++ app/scraper/loot/scraperhelper.py
@@ -6,6 +6,7 @@ from app.scraper.loot.epic_games import EpicGamesScraper
 from app.scraper.loot.gog_games import GogGamesScraper
 from app.scraper.loot.gog_games_alwaysfree import GogGamesAlwaysFreeScraper
 from app.scraper.loot.humble_games import HumbleGamesScraper
+from app.scraper.loot.itch_games import ItchGamesScraper
 from app.scraper.loot.scraper import Scraper
 from app.scraper.loot.steam_games import SteamGamesScraper
 from app.scraper.loot.steam_loot import SteamLootScraper
@@ -21,4 +22,5 @@ def get_all_scrapers() -> list[Type[Scraper]]:
         HumbleGamesScraper,
         SteamGamesScraper,
         SteamLootScraper,
+        ItchGamesScraper,
     ]
