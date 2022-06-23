96c805a516684b80112b19990cc443ffba00031d
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2022-06-23 19:23:09 +0200

    Add telegram command to add announcements (#73)
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

--- app/tools.py
+++ app/tools.py
@@ -1,13 +1,10 @@
 import logging
-from datetime import datetime, timezone
 
 from selenium.webdriver.chrome.webdriver import WebDriver
 from sqlalchemy.orm import Session
 
-from app.common import Channel
 from app.scraper.info.steam import get_steam_details
-from app.sqlalchemy import Announcement, LootDatabase, SteamInfo
-from app.telegram import markdown_escape
+from app.sqlalchemy import SteamInfo
 
 logger = logging.getLogger(__name__)
 
@@ -33,39 +30,3 @@ def refresh_all_steam_info(session: Session, webdriver: WebDriver) -> None:
         steam_info.metacritic_score = new_steam_info.metacritic_score
         steam_info.metacritic_url = new_steam_info.metacritic_url
         steam_info.recommended_price_eur = new_steam_info.recommended_price_eur
-
-
-def add_announcement(session: Session) -> None:
-    """
-    Add an announcement to the database
-    """
-
-    announcement_header = "2022-04-29 - Announcing: Announcements!"
-    announcement_text = (
-        "Yes, that's right. This is an announcement to announce announcements! "
-        "From now on, every time there is something new to announce, you will get a message from me. "
-        "I'll try to keep the spam volume low, though ;-)"
-        "That's it for now! I hope you enjoyed the announcement!"
-    )
-
-    announcement_full = (
-        "*"
-        + markdown_escape(announcement_header)
-        + "*"
-        + "\n\n"
-        + markdown_escape(announcement_text)
-    )
-
-    announcement = Announcement(
-        channel=Channel.TELEGRAM,
-        date=datetime.now().replace(tzinfo=timezone.utc),
-        text_markdown=announcement_full,
-    )
-    try:
-        session.add(announcement)
-        session.commit()
-    except NameError:
-        # No session given
-        with LootDatabase(False) as db:
-            db.Session().add(announcement)
-            db.Session().commit()
