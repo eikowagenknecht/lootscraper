318f22e53790a786aa524ca65d63f350f46612bc
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2022-08-04 22:42:05 +0200

    Fix db upgrade path

--- alembic/versions/20220618_153619_add_offer_duration.py
+++ alembic/versions/20220618_153619_add_offer_duration.py
@@ -5,18 +5,62 @@ Revises: fc43de437432
 Create Date: 2022-06-18 15:36:19.169481+00:00
 
 """
+from datetime import datetime
+from typing import Any
+
 import sqlalchemy as sa
-from sqlalchemy import orm, select
+import sqlalchemy.orm as orm
+from sqlalchemy import Column, Enum, ForeignKey, Integer, String, select
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 from alembic import op
-from app.common import OfferDuration
-from app.sqlalchemy import Offer, TelegramSubscription
+from app.common import OfferDuration, OfferType, Source
+from app.sqlalchemy import AwareDateTime, TelegramSubscription
 
 # revision identifiers, used by Alembic.
 revision = "254444f3560f"
 down_revision = "fc43de437432"
 branch_labels = None
 depends_on = None
+
+Base = declarative_base()  # type: Any
+
+
+class Game(Base):
+    __tablename__ = "games"
+
+    id = sa.Column(sa.Integer, primary_key=True, nullable=False)
+
+    steam_id = sa.Column(sa.Integer)
+    igdb_id = sa.Column(sa.Integer)
+
+    offers = orm.relationship("Offer", back_populates="game")
+
+
+class Offer(Base):
+    """An offer, can be for a game or some other game related content (loot)."""
+
+    __tablename__ = "offers"
+
+    id: int = Column(Integer, primary_key=True, nullable=False)
+    source: Source = Column(Enum(Source), nullable=False)
+    type: OfferType = Column(Enum(OfferType), nullable=False)
+    duration: OfferDuration = Column(Enum(OfferDuration), nullable=False)
+    title: str = Column(String, nullable=False)
+    probable_game_name: str = Column(String, nullable=False)
+
+    seen_first: datetime = Column(AwareDateTime, nullable=False)
+    seen_last: datetime = Column(AwareDateTime, nullable=False)
+    valid_from: datetime | None = Column(AwareDateTime)
+    valid_to: datetime | None = Column(AwareDateTime)
+
+    rawtext: str | None = Column(String)
+    url: str | None = Column(String)
+    img_url: str | None = Column(String)
+
+    game_id: int | None = Column(Integer, ForeignKey("games.id"))
+    game: Game | None = relationship("Game", back_populates="offers")
 
 
 def upgrade() -> None:
