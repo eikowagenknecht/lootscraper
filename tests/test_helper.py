23b4f8dc16b1981e7b1d916f28c22b2b80299eeb
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2022-12-15 21:20:20 +0100

    Refine game name guessing from loot title
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,6 +1,6 @@
 import unittest
 
-from app.scraper.info.utils import get_match_score
+from app.scraper.info.utils import clean_loot_title, get_match_score
 
 
 class LocalTests(unittest.TestCase):
@@ -17,6 +17,42 @@ class LocalTests(unittest.TestCase):
 
         score = get_match_score(search, result)
         self.assertLess(score, 0.99)
+
+    def test_loot_title_cleaning_1(self) -> None:
+        title = "Tom Clancy's Rainbow Six® Siege"
+        cleaned = "Tom Clancy's Rainbow Six® Siege"
+
+        self.assertEqual(clean_loot_title(title), cleaned)
+
+    def test_loot_title_cleaning_2(self) -> None:
+        title = "Lords Mobile: Warlord Pack"
+        cleaned = "Lords Mobile"
+
+        self.assertEqual(clean_loot_title(title), cleaned)
+
+    def test_loot_title_cleaning_3(self) -> None:
+        title = "Mobile Legends: Bang Bang: Amazon Prime Chest"
+        cleaned = "Mobile Legends: Bang Bang"
+
+        self.assertEqual(clean_loot_title(title), cleaned)
+
+    def test_loot_title_cleaning_4(self) -> None:
+        title = "Get up to GTA$400,000 this month in GTA Online"
+        cleaned = "GTA Online"
+
+        self.assertEqual(clean_loot_title(title), cleaned)
+
+    def test_loot_title_cleaning_5(self) -> None:
+        title = "Get up to GTA$400,000 this week in GTA Online"
+        cleaned = "GTA Online"
+
+        self.assertEqual(clean_loot_title(title), cleaned)
+
+    def test_loot_title_cleaning_6(self) -> None:
+        title = "World of Warships — Starter Pack: Dreadnought"
+        cleaned = "World of Warships"
+
+        self.assertEqual(clean_loot_title(title), cleaned)
 
 
 if __name__ == "__main__":
