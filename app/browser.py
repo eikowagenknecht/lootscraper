efec3d7d16b820a25ceb973d01aec360a52bd440
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2022-12-17 23:11:25 +0100

    Remove unneeded browser options
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

--- app/browser.py
+++ app/browser.py
@@ -40,30 +40,8 @@ async def get_browser_context() -> AsyncGenerator[BrowserContext, None]:
                 locale="en-US",
                 # Use Reykjavik timezone (=UTC) because UTC is not supported directly
                 timezone_id="Atlantic/Reykjavik",
-                # Seems not to be needed with PlayWright
-                # user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36"
             )
             context.set_default_timeout(Config.get().web_timeout * 1000)  # Milliseconds
-
-            # TODO: See which of these options (if any) are needed for playwright
-            # # https://stackoverflow.com/a/50725918/1689770
-            # # Must be first argument according to stack overflow
-            # options.add_argument("--no-sandbox")
-            # # ChromeDriver Fixes from https://stackoverflow.com/questions/48450594/selenium-timed-out-receiving-message-from-renderer
-            # # https://stackoverflow.com/a/26283818/1689770
-            # options.add_argument("start-maximized")
-            # # https://stackoverflow.com/a/43840128/1689770
-            # options.add_argument("enable-automation")
-            # # https://stackoverflow.com/a/43840128/1689770
-            # options.add_argument("--disable-infobars")
-            # # https://stackoverflow.com/a/50725918/1689770
-            # options.add_argument("--disable-dev-shm-usage")
-            # # https://stackoverflow.com/a/49123152/1689770
-            # options.add_argument("--disable-browser-side-navigation")
-            # # https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
-            # options.add_argument("--disable-gpu")
-            # # To see everything, we use an extra long window. Default: 1920,1200
-            # options.add_argument("--window-size=1920,1200")
 
             yield context
     finally:
