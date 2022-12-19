3e70141273034ff0a63cd4d171f6c8a5a48b4341
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2022-12-20 00:10:29 +0100

    Tests and tools
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

--- app/tools.py
+++ app/tools.py
@@ -1,10 +1,12 @@
+import json
 import logging
+import re
 
 from playwright.async_api import BrowserContext
 from sqlalchemy.orm import Session
 
 from app.scraper.info.steam import get_steam_details
-from app.sqlalchemy import SteamInfo
+from app.sqlalchemy import LootDatabase, Offer, SteamInfo
 
 logger = logging.getLogger(__name__)
 
@@ -30,3 +32,48 @@ async def refresh_all_steam_info(session: Session, context: BrowserContext) -> N
         steam_info.metacritic_score = new_steam_info.metacritic_score
         steam_info.metacritic_url = new_steam_info.metacritic_url
         steam_info.recommended_price_eur = new_steam_info.recommended_price_eur
+
+
+def harmonize_database(session: Session) -> None:
+    """
+    Harmonize the database by removing duplicates and updating information
+    """
+    logger.info("Harmonizing database")
+    offer: Offer
+    for offer in session.query(Offer):
+        # Replace empty values with correct NULLs
+        if offer.img_url in ("", "None"):
+            logger.info(f"Cleaning up empty image URL for offer {offer.id}")
+            offer.img_url = None
+
+        if offer.rawtext is not None and offer.rawtext.startswith("<"):
+            logger.info(f"Converting rawtext for offer {offer.id}")
+            new_json: dict[str, str] = {}
+            add_xml_element_if_exists(new_json, offer.rawtext, "title")
+            add_xml_element_if_exists(new_json, offer.rawtext, "paragraph")
+            add_xml_element_if_exists(new_json, offer.rawtext, "startdate")
+            add_xml_element_if_exists(new_json, offer.rawtext, "enddate")
+            add_xml_element_if_exists(new_json, offer.rawtext, "appid")
+            add_xml_element_if_exists(new_json, offer.rawtext, "gametitle")
+            add_xml_element_if_exists(new_json, offer.rawtext, "text")
+            offer.rawtext = json.dumps(new_json)
+
+    session.commit()
+
+
+def run_cleanup() -> None:
+    """
+    Run cleanup functions
+    """
+    logger.info("Running cleanup")
+    with LootDatabase(echo=False) as db:
+        harmonize_database(db.Session())
+
+
+def add_xml_element_if_exists(
+    target: dict[str, str], source: str, element: str
+) -> None:
+    try:
+        target[element] = re.search(rf"<{element}>(.*)</{element}>", source)[1]
+    except TypeError:
+        pass
