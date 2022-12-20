82ac453c9a8f19548098150b7458412fed0b4b73
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2022-12-20 17:48:13 +0100

    Add cleanup function for invalid offers
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

--- app/tools.py
+++ app/tools.py
@@ -5,6 +5,7 @@ from playwright.async_api import BrowserContext
 from sqlalchemy.orm import Session
 
 from app.browser import get_browser_context
+from app.common import Category
 from app.database import Game, IgdbInfo, LootDatabase, Offer, SteamInfo
 from lootscraper import add_game_info
 
@@ -26,9 +27,24 @@ async def refresh_all_games(session: Session, context: BrowserContext) -> None:
     all_offers = session.query(Offer).all()
 
     logger.info("Gathering new information")
+    offer: Offer
     for offer in all_offers:
-        logger.info(f"Adding game info for offer {offer.id}")
+        logger.info(f"Adding game info for offer {offer.id}.")
         await add_game_info(offer, session, context)
+
+    session.commit()
+
+
+def delete_invalid_offers(session: Session) -> None:
+    """
+    Delete invalid offers from the database.
+    """
+
+    offer: Offer
+    for offer in session.query(Offer):
+        if offer.category in [Category.DEMO, Category.PRERELEASE]:
+            logger.info(f"Deleting invalid offer {offer.id}.")
+            session.delete(offer)
 
     session.commit()
 
@@ -37,11 +53,11 @@ def fix_image_nones(session: Session) -> None:
     """
     Remove empty image URLs from the database.
     """
+
     offer: Offer
     for offer in session.query(Offer):
-        # Replace empty values with correct NULLs
         if offer.img_url in ("", "None"):
-            logger.info(f"Cleaning up empty image URL for offer {offer.id}")
+            logger.info(f"Cleaning up empty image URL for offer {offer.id}.")
             offer.img_url = None
 
     session.commit()
@@ -49,10 +65,12 @@ def fix_image_nones(session: Session) -> None:
 
 async def run_cleanup() -> None:
     """
-    Run cleanup functions
+    Run cleanup functions.
     """
+
     logger.info("Running cleanup")
     with LootDatabase(echo=False) as db:
+        delete_invalid_offers(db.Session())
         fix_image_nones(db.Session())
 
         async with get_browser_context() as context:
@@ -63,6 +81,7 @@ def cleanup() -> None:
     """
     Synchronous wrapper for cleanup functions. Run this with
     `python -c 'import app.tools; app.tools.cleanup()'` for now.
-    TODO: Add an admin command for this (telegram)
+    TODO: Add an admin command for this (telegram).
     """
+
     asyncio.run(run_cleanup())
