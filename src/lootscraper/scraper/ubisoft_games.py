404b15ff944d621eefe585207f96204e0d8b0fac
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2023-12-03 13:36:21 +0100

    fix: recognize changed ubisoft offer format (#293)

--- src/lootscraper/scraper/ubisoft_games.py
+++ src/lootscraper/scraper/ubisoft_games.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import contextlib
 import logging
 from dataclasses import dataclass
 from datetime import datetime, timezone
@@ -67,20 +68,24 @@ class UbisoftGamesScraper(Scraper):
         # Scroll element into view to load img url
         await element.scroll_into_view_if_needed()
 
+        # Format (values in <> are an example):
+        # Get <Assassin's Creed> for free!
         title = await element.locator(".c-focus-banner__title").text_content()
         if title is None:
             raise ValueError("Couldn't find title.")
 
-        if "free" not in title.lower():
+        # Filter out various other promotions
+        if not title.startswith("Get ") or not title.endswith(" for FREE!"):
             return None
 
-        # Format: January 23, 2023 at 3PM UTC
+        # Format (values in <> are an example):
+        # Offer ends <January 23, 2023 at 3PM UTC>.
         valid_to = await element.locator(".c-focus-banner__legal-line").inner_text()
 
         if valid_to is None:
             raise ValueError(f"Couldn't find valid to for {title}.")
 
-        url = await element.locator("a").get_attribute("href")
+        url = await element.locator("a.button").get_attribute("href")
         if url is None:
             raise ValueError(f"Couldn't find url for {title}.")
         if not url.startswith("http"):
@@ -112,15 +117,30 @@ class UbisoftGamesScraper(Scraper):
         }
 
         title = raw_offer.title.removeprefix("Get ").removesuffix(" for FREE!")
-        valid_to = raw_offer.valid_to.removeprefix("Offer valid until ").removesuffix(
-            " UTC.",
+        valid_to = (
+            raw_offer.valid_to.removeprefix("Offer valid until ")
+            .removeprefix("Offer ends ")
+            .removesuffix(
+                " UTC.",
+            )
         )
 
         utc_valid_to = None
         if valid_to:
-            utc_valid_to = datetime.strptime(valid_to, "%B %d, %Y at %I%p").replace(
-                tzinfo=timezone.utc,
-            )
+            with contextlib.suppress(ValueError):
+                utc_valid_to = datetime.strptime(valid_to, "%B %d, %Y at %I%p").replace(
+                    tzinfo=timezone.utc,
+                )
+
+        # Fallback for weird date format "January 23 at 2023 at 3PM UTC", seen 2023-12
+        if valid_to and not utc_valid_to:
+            with contextlib.suppress(ValueError):
+                utc_valid_to = datetime.strptime(
+                    valid_to,
+                    "%B %d at %Y at %I%p",
+                ).replace(
+                    tzinfo=timezone.utc,
+                )
 
         return Offer(
             source=UbisoftGamesScraper.get_source(),
