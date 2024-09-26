74077b26ffe65556e878940d98ddb80b0b3374fa
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2024-09-26 17:00:27 +0200

    fix: add debug logging for scrolling
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

--- src/lootscraper/scraper/itch_games.py
+++ src/lootscraper/scraper/itch_games.py
@@ -56,7 +56,7 @@ class ItchGamesScraper(Scraper):
         ]
 
     async def page_loaded_hook(self, page: Page) -> None:
-        await Scraper.scroll_page_to_bottom(page)
+        await Scraper.scroll_page_to_bottom(self, page)
 
     async def read_raw_offer(self, element: Locator) -> RawOffer:
         # Scroll into view to mage sure the image is loaded
