3cb2a4623761ae1c133f119b5d6d25cc2bde94ab
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2024-12-06 23:49:19 +0100

    style: lint
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import { logger } from '@/utils/logger';
+import { logger } from "@/utils/logger";
 
+// eslint-disable-next-line @typescript-eslint/require-await
 async function main() {
-  logger.info('Starting LootScraper...');
+  logger.info("Starting LootScraper...");
   // TODO: Implement
 }
 
-main().catch((error) => {
-  logger.error('Fatal error:', error);
+main().catch((error: unknown) => {
+  logger.error("Fatal error:", error);
   process.exit(1);
 });
