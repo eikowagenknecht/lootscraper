11b00acb1db79f061d3463273992e4c300116c9e
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2024-12-10 21:24:12 +0100

    chore: update vitest config
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from "node:path";
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
@@ -17,10 +17,16 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "node",
+    include:
+      mode === "contract"
+        ? ["src/**/*.contract.test.{ts,tsx}"] // Only contract tests
+        : ["src/**/*.{test,spec}.{ts,tsx}", "!src/**/*.contract.test.{ts,tsx}"],
+    exclude: ["node_modules/**", "dist/**", "**/types/**", "data/**"],
+    testTimeout: mode === "contract" ? 10000 : 5000,
     coverage: {
       provider: "v8",
       reporter: ["text", "html"],
-      exclude: ["**/node_modules/**", "**/dist/**", "**/types/**"],
+      exclude: ["node_modules/**", "dist/**", "**/types/**"],
     },
   },
-});
+}));
