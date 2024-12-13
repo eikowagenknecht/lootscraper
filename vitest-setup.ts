92e7ee6a1d767b7e5b9ecb368bc7333789c7adff
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2024-12-13 13:47:58 +0100

    feat: set fixed reference date for luxon in test setup
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -1,6 +1,11 @@
 import "@testing-library/jest-dom/vitest";
-import { Settings as LuxonSettings } from "luxon";
+import { DateTime, Settings as LuxonSettings } from "luxon";
 
 // Configure Luxon globally for all tests
 LuxonSettings.defaultZone = "utc";
 LuxonSettings.throwOnInvalid = true;
+
+// Set a fixed start time for all tests
+const referenceDate = 1733055717000; // 2024-12-01T12:21:57.000Z
+const offset = DateTime.now().toMillis() - referenceDate;
+LuxonSettings.now = () => DateTime.now().toMillis() - offset; // 2024-12-01T12:21:57.000Z
