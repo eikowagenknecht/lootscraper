5383a4f5701110d5251cfb854d73f9193447e971
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2024-12-24 09:13:37 +0100

    refactor: move Category enum
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

--- src/scrapers/index.ts
+++ src/scrapers/index.ts
@@ -4,7 +4,6 @@ export * from "./implementations";
 // Export base types
 export { BaseScraper } from "./base/scraper";
 export type { OfferHandler, RawOffer } from "./base/scraper";
-export { Category } from "./base/scraper";
 
 // Export utilities
 export { getEnabledScraperCombinations } from "./utils";
