2436964cd3b78401dbda40ef443d0558658b3e80
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2025-02-06 13:06:20 +0100

    refactor: merge telegrambot with telegrambotservice
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

--- src/services/telegrambot/types/config.ts
+++
@@ -1,8 +0,0 @@
-import type { TelegramLogLevel } from "@/types";
-
-export interface BotConfig {
-  accessToken: string;
-  botLogChatId: number;
-  botOwnerUserId: number;
-  logLevel: TelegramLogLevel;
-}
