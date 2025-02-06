ea9a1450c94a3b6a74a459c8fb600014f4316f81
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2025-02-06 13:13:03 +0100

    refactor: remove unneeded type files
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

--- src/services/scraper/index.ts
+++
@@ -1,6 +0,0 @@
-// Re-export implementations
-export * from "./implementations";
-
-// Export base types
-export { BaseScraper } from "./base/scraper";
-export type { OfferHandler, RawOffer } from "./base/scraper";
