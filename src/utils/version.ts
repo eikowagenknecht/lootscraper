894b418406ff9358f90a34965a5df4df752d4760
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2025-02-07 08:52:30 +0100

    feat: add version number to startup logging
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

---
+++ src/utils/version.ts
@@ -0,0 +1,22 @@
+import { readFile } from "node:fs/promises";
+import { logger } from "./logger";
+import { getPackageJsonPath } from "./path";
+
+interface PackageJson {
+  version: string;
+  name: string;
+}
+
+export async function getPackageInfo(): Promise<PackageJson> {
+  try {
+    const packageJsonPath = getPackageJsonPath();
+    const packageJson = await readFile(packageJsonPath, "utf-8");
+    return JSON.parse(packageJson) as PackageJson;
+  } catch (error) {
+    logger.error("Failed to read package.json", { error });
+    return {
+      version: "unknown",
+      name: "lootscraper",
+    };
+  }
+}
